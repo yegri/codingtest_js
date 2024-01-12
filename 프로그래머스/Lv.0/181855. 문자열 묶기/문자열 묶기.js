44d019ed85a37ea588ba0f50bdb64c09135bf7ec
yegri <113401733+yegri@users.noreply.github.com>
2024-01-12 14:08:26 +0900

    [level Lv.0] Title: 문자열 묶기, Time: 10.22 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181855. 문자열 묶기/문자열 묶기.js
+++ 프로그래머스/Lv.0/181855. 문자열 묶기/문자열 묶기.js
@@ -1,19 +1,26 @@
 function solution(strArr) {
+    // 배열의 길이를 넣어주기 위한 배열 생성
     let arr = []
     
+    // 배열 길이 넣어주기
     for (let str of strArr){
        arr.push(str.length)
     }
     
+    // 길이 개수 구하기 위한 객체 생성
     const result = {};
     
+    
+    // 길이 개수 구하기
     arr.forEach((x) => { 
       result[x] = (result[x] || 0)+1; 
     });
     
+    // 객체에서 key만 빼내서 새로운 배열 만들기
     let arr2 = Object.keys(result).map(function (key) { return result[key]; });
+    
+    // 새로운 배열에서 가장 큰 값 찾기
     let max = Math.max.apply(null, arr2);
     
     return max
-   
 }
