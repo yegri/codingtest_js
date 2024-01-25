2d00fa230db97393882e0a3d83c377ffaae4cd90
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-01-25 19:16:09 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,16 +1,9 @@
 function solution(numbers) {
+    let N = numbers.length
     numbers.sort((a,b) => b - a)
     
-    let answer = 1
-    const minus = numbers.filter(x => x < 0).length
-    
-    if (minus >= 2 && (numbers[numbers.length-1] * numbers[numbers.length-2] > numbers[0] * numbers[1] )){
-        answer = numbers[numbers.length-1] * numbers[numbers.length-2]
-    }else{
-        answer = numbers[0] * numbers[1]
-    }
-    
-    console.log(numbers)
-    
-    return answer
+    return Math.max(
+        numbers[N-1] * numbers[N-2],
+        numbers[0] * numbers[1]
+    )
 }
