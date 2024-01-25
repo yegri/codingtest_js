0891ebd7ff78e04af14a08dbabf6b55e787f8356
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-01-25 18:55:11 +0900

    [level 0] Title: 암호 해독, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,11 @@
+function solution(cipher, code) {
+    let answer = ''
+    
+    for(let i = code; i <= cipher.length; i += code){
+        
+            answer += cipher[i - 1]
+        
+    }
+    
+    return answer
+}
