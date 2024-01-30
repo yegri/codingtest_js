6f3b228de1d4a54107f2a8d6d2f626ade4151e65
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-01-30 12:20:09 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let arr = []
+    
+    for(let i = 2; i <= n; i++){
+        while(n % i === 0){
+            arr.push(i)
+            n = n / i
+        }
+    }
+    
+    return [...new Set(arr)]
+}
