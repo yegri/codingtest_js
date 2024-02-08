e3ded5632d32b6b7ce1e273d4514b7f9effe6fa1
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-02-08 16:58:59 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 4.50 ms, Memory: 36.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    let answer = []
+    
+    for(let i = 1; i < n; i++){
+        if (n % i === 1){
+            answer.push(i)
+        } 
+    }
+    
+    return answer[0]
+}
