772097d8bd3fc64030e85bd6ee487bd96dd45486
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-02-08 14:24:24 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,9 +1,17 @@
 function solution(lines) {
-    let line = new Array(200).fill(0)
+    // 최초로 그려지는 선분의 시작점
+    const start = Math.min(...lines.flat())
+    // 마지막에 그려지는 선분의 종단점
+    const end = Math.max(...lines.flat())
+    // 차이만큼 0으로 채우기
+    const list = [...Array(end-start)].fill(0)
     
-    lines.forEach(([a,b]) => {
-        for(; a < b; a++) line[a+100]++
+    lines.forEach(el => {
+        for (let i = el[0]; i < el[1]; i++){
+            list[i-start] += 1 // 1씩 더해주기
+        }
     })
     
-    return line.reduce((a,c) => c > 1 ? a + 1 : a, 0)
+    // 선분들 다 더했을 때 리스트에 2 이상이면 겹친부분
+    return list.reduce((a,c) => c > 1 ? a + 1 : a , 0)
 }
