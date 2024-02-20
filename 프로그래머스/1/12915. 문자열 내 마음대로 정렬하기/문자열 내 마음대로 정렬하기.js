95b68b65c8b58bd08a74f46524c3b112d803920e
Lee Yeeun <113401733+yegri@users.noreply.github.com>
2024-02-20 14:43:25 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,9 @@
+function solution(strings, n) {
+    let newStrings = strings.sort((a,b) => {
+        if(a[n] > b[n]) return 1 
+        if(a[n] < b[n]) return -1 
+        if (a[n] == b[n]) return (a > b ? 1 : -1)
+    })
+    
+    return newStrings
+}
